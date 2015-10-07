7f99bdc18c2e46306b086f9da7bbb49ee75fcd24
Brian Xu <bwxu@mit.edu>
2015-10-07 00:04:57 -0400

    Added recursive DFS

--- DFSBFS.py
+++ DFSBFS.py
@@ -5,50 +5,56 @@ class Node:
 		self.parent = None
 		
 def DFS(start):
-	visitedNodes = set()
+	visitedNodes = {start: 0}
 	stack = [start]
 	path = []
-	topoPath = []
-	t = 0
+	count = 0
 	
 	while len(stack) > 0:
 		node = stack.pop()
-		if node in visitedNodes:
-			continue
-		
 		path.append(node.value)
-		visitedNodes.add(node)
 		
-		numChildren = 0
 		for child in node.children:
 			if child not in visitedNodes:
 				stack.append(child)
-				numChildren += 1
-				
-		if numChildren == 0:
-			topoPath.append(node.value)	
-		t += 1
-		
-	print topoPath[::-1]
-	return path, t
+				count += 1
+				visitedNodes[child] = count
+	print visitedNodes
+	return path
 	
 def BFS(start):
-	visitedNodes = set()
+	visitedNodes = {start: 0}
 	queue = [start]
 	path = []
 	
 	while len(queue) > 0:
 		node = queue.pop(0)
-		if node in visitedNodes:
-			continue
-		
 		path.append(node.value)
-		visitedNodes.add(node)
 		
 		for child in node.children:
 			if child not in visitedNodes:
 				queue.append(child)
+				visitedNodes[child] = visitedNodes[node] + 1
+
 	return path
+	
+def DFSRecursive(start):
+	parents = {}
+	finish = []
+	dfsVisit(start, parents, finish)
+	return finish
+	
+def dfsVisit(start, parents, finish):
+	for child in start.children:
+		if child not in parents:
+			parents[child] = start
+			dfsVisit(child, parents, finish)
+	finish.append(start.value)
+	
+def topologicalOrder(start):
+	dfsResult = DFSRecursive(start)
+	dfsResult.reverse()
+	return dfsResult
 	
 graph = Node(1)
 graph.children += [Node(2), Node(3), Node(4)]
@@ -57,3 +63,7 @@ graph.children[0].children += [Node(5), Node(6)]
 print DFS(graph)
 
 print BFS(graph)
+
+print DFSRecursive(graph)
+
+print topologicalOrder(graph)
